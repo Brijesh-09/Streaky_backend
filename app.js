18fedebdbe1e59151a41b5c2e06ebd6066ec301a
Brijesh Kori <brijeshkori@Brijeshs-MacBook-Air.local>
2024-11-20 11:54:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+const authRoutes = require('./routes/auth');
+const protectedRoutes = require('./routes/protected');
+
+app.use('/auth', authRoutes);
+app.use('/protected', protectedRoutes);
+
+// MongoDB Connection
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Database connected");
+    } catch (error) {
+        console.error("Database connection failed:", error);
+        process.exit(1); 
+    }
+};
+
+connectToDatabase();
+// Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
